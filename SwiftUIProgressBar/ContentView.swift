767096b36845aa49418b76ed6576212c5ec42218
Elliott Kung <31856394+EllKuu@users.noreply.github.com>
2022-10-23 15:39:12 -0400

    Initial Commit

---
+++ SwiftUIProgressBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIProgressBar
+//
+//  Created by elliott kung on 2022-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIProgressBar/SwiftUIProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgressBarApp.swift
+//  SwiftUIProgressBar
+//
+//  Created by elliott kung on 2022-10-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
